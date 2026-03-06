d42c2f1afdcf62a8f11add2767a6b63377dfc6cd
Emanuel Georgi <georgi_emi30@yahoo.com>
2026-03-06 20:55:01 +0200

    Added Book microservice and documented it using Swagger UI and MicroProfile OpenAPI

--- rest-book/src/test/java/org/agoncal/quarkus/microservices/book/BookResourceTest.java
+++ rest-book/src/test/java/org/agoncal/quarkus/microservices/book/BookResourceTest.java
@@ -5,16 +5,28 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.startsWith;
 
 @QuarkusTest
 class BookResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void shouldCreateABook() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .formParam("title", "Understanding Quarkus")
+                .formParam("author", "Antonio")
+                .formParam("year", 2021)
+                .formParam("genre", "IT").
+        when()
+                .post("/api/books").
+        then()
+                .statusCode(201)
+                .body("ISBN_13", startsWith("13-"))
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio"))
+                .body("year_of_publication", is(2021))
+                .body("genre", is("IT"))
+                .body("creation_date", startsWith("20"));
+
     }
 
 }
